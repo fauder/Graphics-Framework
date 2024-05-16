b9d345cb6f9fec46a18f94a01db3d459200f8e67
fauder <canik.burak@gmail.com>
2024-05-16 15:01:11 +0300

    Sandbox Application: Add vertex color attributes

--- Sandbox/Source/SandboxApplication.cpp
+++ Sandbox/Source/SandboxApplication.cpp
@@ -29,16 +29,22 @@ void SandboxApplication::Initialize()
 	Platform::ChangeTitle( "Sandbox (Graphics Framework)" );
 
 	// Set-up triangle data.
-	float rectangle_vertices[] = {
-		 0.5f,  0.5f, 0.0f, // Top right.
-		 0.5f, -0.5f, 0.0f, // Bottom right.
-		-0.5f, -0.5f, 0.0f, // Bottom left.
-		-0.5f,  0.5f, 0.0f  // Top left.
+	//float rectangle_vertices[] = {
+	//	 0.5f,  0.5f, 0.0f, // Top right.
+	//	 0.5f, -0.5f, 0.0f, // Bottom right.
+	//	-0.5f, -0.5f, 0.0f, // Bottom left.
+	//	-0.5f,  0.5f, 0.0f  // Top left.
+	//};
+	vertices = {
+		/* Position:			Color: */
+		 0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,	// Bottom right.
+		-0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,	// Bottom left.
+		 0.0f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f	// Top right.
 	};
-	unsigned int rectangle_indices[] =
+	indices =
 	{
 		0, 1, 3, // First triangle.
-		1, 2, 3  // Second triangle.
+		//1, 2, 3  // Second triangle.
 	};
 
 	// Create & bind the vertex array object.
@@ -49,17 +55,20 @@ void SandboxApplication::Initialize()
 	unsigned int vertex_buffer_object;
 	GLCALL( glGenBuffers( 1, &vertex_buffer_object ) );
 	GLCALL( glBindBuffer( GL_ARRAY_BUFFER, vertex_buffer_object ) );
-	GLCALL( glBufferData( GL_ARRAY_BUFFER, sizeof( rectangle_vertices ), &rectangle_vertices, GL_STATIC_DRAW ) );
+	GLCALL( glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices.data(), GL_STATIC_DRAW ) );
 
 	// Create & bind the element buffer object.
 	unsigned int element_buffer_object;
 	GLCALL( glGenBuffers( 1, &element_buffer_object ) );
 	GLCALL( glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, element_buffer_object ) );
-	GLCALL( glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( rectangle_indices ), &rectangle_indices, GL_STATIC_DRAW ) );
+	GLCALL( glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices.data(), GL_STATIC_DRAW ) );
 
 	// Define vertex attribute pointers & enable them.
-	GLCALL( glVertexAttribPointer( /* vertex attrib. location: */ 0, /* vector3 so -> */ 3, GL_FLOAT, /* Do not normalize */ GL_FALSE, sizeof( float ) * 3, nullptr ) );
+	constexpr unsigned int vertex_stride = ( 3 + 3 ) * sizeof( float );
+	GLCALL( glVertexAttribPointer( /* vertex attrib. location: */ 0, /* vector3 so -> */ 3, GL_FLOAT, /* Do not normalize */ GL_FALSE, vertex_stride, nullptr ) );
 	GLCALL( glEnableVertexAttribArray( 0 ) );
+	GLCALL( glVertexAttribPointer( /* vertex attrib. location: */ 1, /* vector3 so -> */ 3, GL_FLOAT, /* Do not normalize */ GL_FALSE, vertex_stride, ( char* )0 + 12 ) );
+	GLCALL( glEnableVertexAttribArray( 1 ) );
 
 	GLCALL( glBindBuffer( GL_ARRAY_BUFFER, 0 ) ); // It is safe to unbind the VBO as the glVertexAttribPointer call above registered the VBO to the VAO.
 	
@@ -73,14 +82,17 @@ void SandboxApplication::Initialize()
 	const char* const vertex_shader_source =
 		"#version 330 core\n"
 		"layout ( location = 0 ) in vec3 position;\n"
+		"layout ( location = 1 ) in vec3 color;\n"
+		"out vec3 vertex_color;\n"
 		"\n"
 		"void main()\n"
 		"{\n"
 		"	gl_Position = vec4( position.x, position.y, position.z, 1.0f );\n"
+		"	vertex_color = color;\n"
 		"}\n";
 
 	unsigned int vertex_shader;
-	vertex_shader = glCreateShader( GL_VERTEX_SHADER );
+	GLCALL( vertex_shader = glCreateShader( GL_VERTEX_SHADER ) );
 	GLCALL( glShaderSource( vertex_shader, /* how many strings: */ 1, &vertex_shader_source, NULL ) );
 	GLCALL( glCompileShader( vertex_shader ) );
 
@@ -91,22 +103,25 @@ void SandboxApplication::Initialize()
 		char info_log[ 512 ];
 		glGetShaderInfoLog( vertex_shader, 512, NULL, info_log );
 		std::cerr << "ERROR::SHADER::VERTEX::COMPILE:" << info_log << "\n";
+		throw std::logic_error( std::string( "ERROR::SHADER::VERTEX::COMPILE:" ) + info_log );
 		return;
 	}
 	
 	// Create & compile the fragment shader.
 	const char* const fragment_shader_source =
 		"#version 330 core\n"
-		"uniform vec4 final_color;\n"
+		"in vec3 vertex_color;\n"
 		"out vec4 color;\n"
+		"\n"
+		"uniform vec4 final_color;\n"
 		"\n"
 		"void main()\n"
 		"{\n"
-		"	color = final_color;\n"
+		"	color = vec4( vertex_color, 1.0f );\n"
 		"}\n";
 
 	unsigned int fragment_shader;
-	fragment_shader = glCreateShader( GL_FRAGMENT_SHADER );
+	GLCALL( fragment_shader = glCreateShader( GL_FRAGMENT_SHADER ) );
 	GLCALL( glShaderSource( fragment_shader, /* how many strings: */ 1, &fragment_shader_source, NULL ) );
 	GLCALL( glCompileShader( fragment_shader ) );
 
@@ -116,6 +131,7 @@ void SandboxApplication::Initialize()
 		char info_log[ 512 ];
 		glGetShaderInfoLog( fragment_shader, 512, NULL, info_log );
 		std::cerr << "ERROR::SHADER::FRAGMENT::COMPILE:" << info_log << "\n";
+		throw std::logic_error( std::string( "ERROR::SHADER::FRAGMENT::COMPILE:" ) + info_log );
 		return;
 	}
 
@@ -130,6 +146,7 @@ void SandboxApplication::Initialize()
 		char info_log[ 512 ];
 		glGetShaderInfoLog( shader_program, 512, NULL, info_log );
 		std::cerr << "ERROR::SHADER::PROGRAM::LINK:" << info_log << "\n";
+		throw std::logic_error( std::string( "ERROR::SHADER::PROGRAM::LINK:" ) + info_log );
 		return;
 	}
 	
@@ -155,23 +172,23 @@ void SandboxApplication::Render()
 
 	const auto current_time = Platform::GetCurrentTime();
 
-	int uniform_location = glGetUniformLocation( shader_program, "final_color" );
+	GLCALL( int uniform_location = glGetUniformLocation( shader_program, "final_color" ) );
 	float red_value = ( cos( current_time ) / 2.0f ) + 0.5f;
 	float green_value = ( sin( current_time ) / 2.0f ) + 0.5f;
 
 	GLCALL( glUseProgram( shader_program ) );
-	glUniform4f( uniform_location, red_value, green_value, 0.0f, 1.0f );
+	GLCALL( glUniform4f( uniform_location, red_value, green_value, 0.0f, 1.0f ) );
 
 	GLCALL( glBindVertexArray( vertex_array_object ) );
-	//GLCALL( glDrawArrays( GL_TRIANGLES, 0, 3 ) );
-	GLCALL( glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr ) );
+	//GLCALL( glDrawArrays( GL_TRIANGLES, 0, ( int )vertices.size() ) );
+	GLCALL( glDrawElements( GL_TRIANGLES, ( int )indices.size(), GL_UNSIGNED_INT, nullptr ) );
 }
 
 void SandboxApplication::DrawImGui()
 {
 	if( ImGui::Begin( "Test Window" ) )
 	{
-		ImGui::Text( "Running Sandbox Application:\n\nDrawing an indexed rectangle!" );
+		ImGui::Text( "Running Sandbox Application:\n\nDrawing a colorful triangle\nwith vertex colors!" );
 	}
 
 	ImGui::End();
--- Sandbox/Source/SandboxApplication.h
+++ Sandbox/Source/SandboxApplication.h
@@ -21,6 +21,9 @@ public:
 	//virtual void OnKeyboardEvent( const Platform::KeyCode key_code, const Platform::KeyAction key_action, const Platform::KeyMods key_mods ) override;
 
 private:
+	std::array< float, 3 * ( 3 + 3 ) > vertices;
+	std::array< unsigned int, 3 > indices;
+
 	unsigned int vertex_array_object;
 	unsigned int shader_program;
 };
