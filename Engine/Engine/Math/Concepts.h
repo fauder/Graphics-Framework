1cb56f6b9f3148ec1144feee7fb44e04683005a5
fauder <canik.burak@gmail.com>
2024-06-03 15:56:04 +0300

    Engine Math/Concepts: Add concept 'IsAnyOf'

--- Engine/Engine/Math/Concepts.h
+++ Engine/Engine/Math/Concepts.h
@@ -8,4 +8,6 @@ namespace Engine::Concepts
 	template< typename T > concept Arithmetic = std::integral< T > || std::floating_point< T >;
 
 	template< std::size_t value > concept NonZero = ( value != 0 );
+
+	template< typename T, typename ... U > concept IsAnyOf = ( std::same_as< T, U > || ... );
 }
