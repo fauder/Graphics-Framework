6afd3dbc54ba9136e6dc3c41af4d588c8348e1a2
fauder <canik.burak@gmail.com>
2024-06-20 19:08:39 +0300

    Engine/Core/Utility: Add String functions to strip whitespace & parsing the previous word given an offset

--- Engine/Engine/Core/Utility.cpp
+++ Engine/Engine/Core/Utility.cpp
@@ -28,5 +28,41 @@ namespace Engine
 				return std::nullopt;
 			}
 		}
+
+		namespace String
+		{
+			std::string_view RemoveLeadingWhitespace( const std::string_view source )
+			{
+				return std::string_view( source.cbegin() + source.find_first_not_of( " \t" ), source.cend() );
+			}
+
+			std::string_view RemoveTrailingWhitespace( const std::string_view source )
+			{
+				if( source.back() == ' ' || source.back() == '\t' )
+					return std::string_view( source.cbegin(), source.cbegin() + source.find_last_not_of( " \t" ) );
+
+				return source; // No trailing whitespace found.
+			}
+
+			std::string_view FindPreviousWord( const std::string_view& source, const std::size_t offset )
+			{
+				const std::size_t last_char_pos = source.find_last_not_of( " \t", offset - 1 );
+
+				if( last_char_pos != std::string::npos )
+				{
+					const std::size_t last_preceding_whitespace_pos = source.find_last_of( " \t", last_char_pos );
+
+					if( last_preceding_whitespace_pos != std::string::npos )
+					{
+						return std::string_view( source.cbegin() + last_preceding_whitespace_pos + 1, source.begin() + last_char_pos + 1 );
+					}
+
+					// No whitespace found before the word.
+					return std::string_view( source.cbegin(), source.begin() + last_char_pos + 1 );
+				}
+
+				return source; // Contains only white-space.
+			}
+		}
 	}
 }
--- Engine/Engine/Core/Utility.h
+++ Engine/Engine/Core/Utility.h
@@ -3,12 +3,20 @@
 // std Includes.
 #include <optional>
 #include <string>
+#include <string_view>
 
 namespace Engine
 {
 	namespace Utility
 	{
 		std::optional< std::string > ReadFileIntoString( const char* file_path, const char* optional_error_prompt = nullptr );
+
+		namespace String
+		{
+			std::string_view RemoveLeadingWhitespace( const std::string_view source );
+			std::string_view RemoveTrailingWhitespace( const std::string_view source );
+			std::string_view FindPreviousWord( const std::string_view& source, const std::size_t offset );
+		}
 	};
 }
 
