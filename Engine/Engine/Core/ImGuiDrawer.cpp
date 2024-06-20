ebf2fc9007c21ae69333c2b8aee8a0cbfaf4e25f
fauder <canik.burak@gmail.com>
2024-06-20 20:01:59 +0300

    🔁 Engine/Core/ImGuiDrawer::Draw( Shader ): Put each shader under a tree node & remove the general info. section

--- Engine/Engine/Core/ImGuiDrawer.cpp
+++ Engine/Engine/Core/ImGuiDrawer.cpp
@@ -16,40 +16,36 @@ namespace Engine::ImGuiDrawer
 
 	void Draw( const Shader& shader, ImGuiWindowFlags window_flags )
 	{
-		if( ImGui::Begin( "Shader Info.", nullptr, window_flags | ImGuiWindowFlags_AlwaysAutoResize ) )
+		if( ImGui::Begin( "Shaders", nullptr, window_flags | ImGuiWindowFlags_AlwaysAutoResize ) )
 		{
 			const auto& uniform_map = shader.GetUniformInformations();
 
-			ImGui::SeparatorText( "General Information" );
-			if( ImGui::BeginTable( "General", 2, ImGuiTableFlags_Borders | ImGuiTableFlags_SizingStretchProp | ImGuiTableFlags_PreciseWidths ) )
+			if( ImGui::TreeNodeEx( shader.GetName().c_str(), ImGuiTreeNodeFlags_DefaultOpen | ImGuiTreeNodeFlags_Framed ) )
 			{
-				ImGui::TableNextColumn(); ImGui::TextUnformatted( "Shader Name" );
-				ImGui::TableNextColumn(); ImGui::Text( R"("%s")", shader.GetName().c_str() );
-
-				ImGui::EndTable();
-			}
-
-			ImGui::SeparatorText( "Uniforms" );
-			if( ImGui::BeginTable( "Uniforms", 5, ImGuiTableFlags_Borders | ImGuiTableFlags_SizingStretchProp | ImGuiTableFlags_PreciseWidths ) )
-			{
-				ImGui::TableSetupColumn( "Name" );
-				ImGui::TableSetupColumn( "Location" );
-				ImGui::TableSetupColumn( "Size" );
-				ImGui::TableSetupColumn( "Offset" );
-				ImGui::TableSetupColumn( "Type" );
-
-				ImGui::TableHeadersRow();
-
-				for( auto& [ name, uniform_info ] : uniform_map )
+				ImGui::SeparatorText( "Uniforms" );
+				if( ImGui::BeginTable( "Uniforms", 5, ImGuiTableFlags_Borders | ImGuiTableFlags_SizingStretchProp | ImGuiTableFlags_PreciseWidths ) )
 				{
-					ImGui::TableNextColumn(); ImGui::TextUnformatted( name.c_str() );
-					ImGui::TableNextColumn(); ImGui::Text( "%d", uniform_info.location );
-					ImGui::TableNextColumn(); ImGui::Text( "%d", uniform_info.size );
-					ImGui::TableNextColumn(); ImGui::Text( "%d", uniform_info.offset );
-					ImGui::TableNextColumn(); ImGui::TextUnformatted( uniform_info.IsUserDefinedStruct() ? "struct" : GetNameOfType(uniform_info.type));
+					ImGui::TableSetupColumn( "Name" );
+					ImGui::TableSetupColumn( "Location" );
+					ImGui::TableSetupColumn( "Size" );
+					ImGui::TableSetupColumn( "Offset" );
+					ImGui::TableSetupColumn( "Type" );
+
+					ImGui::TableHeadersRow();
+
+					for( auto& [ name, uniform_info ] : uniform_map )
+					{
+						ImGui::TableNextColumn(); ImGui::TextUnformatted( name.c_str() );
+						ImGui::TableNextColumn(); ImGui::Text( "%d", uniform_info.location );
+						ImGui::TableNextColumn(); ImGui::Text( "%d", uniform_info.size );
+						ImGui::TableNextColumn(); ImGui::Text( "%d", uniform_info.offset );
+						ImGui::TableNextColumn(); ImGui::TextUnformatted( uniform_info.IsUserDefinedStruct() ? "struct" : GetNameOfType(uniform_info.type));
+					}
+
+					ImGui::EndTable();
 				}
 
-				ImGui::EndTable();
+				ImGui::TreePop();
 			}
 		}
 
