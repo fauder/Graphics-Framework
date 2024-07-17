a67637881d85ee6bc3b9cf79d1b9f54611c014b8
fauder <canik.burak@gmail.com>
2024-07-18 02:41:15 +0300

    Engine/Graphics/Texture: Add ID getter

--- Engine/Engine/Graphics/Texture.h
+++ Engine/Engine/Graphics/Texture.h
@@ -20,7 +20,8 @@ namespace Engine
 		bool FromFile( const char* file_path, const int format );
 
 	/* Queries: */
-		inline const std::string& Name() const { return name; }
+		inline const std::string&	Name()	const { return name;	}
+		inline const ID				Id()	const { return id;		}
 
 	/* Usage: */
 		void Activate( const int slot );
