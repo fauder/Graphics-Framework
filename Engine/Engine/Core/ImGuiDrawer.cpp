a83ba30da70ee0448c1d3c0de02a008ec97bd36d
fauder <canik.burak@gmail.com>
2024-08-19 21:46:57 +0300

    🐞 Engine/Core/ImGuiDrawer::Draw( Material& ): UBO members could not be edited properly

--- Engine/Engine/Core/ImGuiDrawer.cpp
+++ Engine/Engine/Core/ImGuiDrawer.cpp
@@ -384,11 +384,10 @@ namespace Engine::ImGuiDrawer
 						if( ImGui::TreeNodeEx( uniform_buffer_name.c_str(), ImGuiTreeNodeFlags_Framed ) )
 						{
 							ImGui::TableNextRow();
-							/* No need to update the Material when the Draw() call below returns true; Memory from the blob is provided directly to Draw(), so the Material is updated. */
 
 							std::byte* memory_blob = ( std::byte* )material.Get( uniform_buffer_name );
 
-							for( const auto& [ dont_care, uniform_buffer_member_struct_info ] : uniform_buffer_info->members_struct_map )
+							for( const auto& [ uniform_buffer_member_struct_name, uniform_buffer_member_struct_info ] : uniform_buffer_info->members_struct_map )
 							{
 								ImGui::TableNextColumn();
 
@@ -402,8 +401,13 @@ namespace Engine::ImGuiDrawer
 
 										ImGui::TableNextColumn();
 
+										std::byte* effective_offset = memory_blob + uniform_buffer_member_info->offset;
+
 										ImGui::PushID( ( void* )uniform_buffer_member_info );
-										is_modified |= Draw( uniform_buffer_member_info->type, memory_blob + uniform_buffer_member_info->offset );
+										is_modified |= Draw( uniform_buffer_member_info->type, effective_offset );
+										/* Draw() call above modifies the memory provided but an explicit material.SetPartial_Struct() call is necessary for proper registration of the modification. */
+										if( is_modified )
+											material.SetPartial_Struct( uniform_buffer_name, uniform_buffer_member_struct_name.c_str(), effective_offset );
 										ImGui::PopID();
 									}
 
@@ -413,7 +417,7 @@ namespace Engine::ImGuiDrawer
 									ImGui::TableNextRow();
 							}
 
-							for( const auto& [ dont_care, uniform_buffer_member_array_info ] : uniform_buffer_info->members_array_map )
+							for( const auto& [ uniform_buffer_member_array_name, uniform_buffer_member_array_info ] : uniform_buffer_info->members_array_map )
 							{
 								ImGui::TableNextColumn();
 
@@ -442,6 +446,9 @@ namespace Engine::ImGuiDrawer
 
 												ImGui::PushID( effective_offset );
 												is_modified |= Draw( uniform_buffer_member_info->type, effective_offset );
+												/* Draw() call above modifies the memory provided but an explicit material.SetPartial_Array() call is necessary for proper registration of the modification. */
+												if( is_modified )
+													material.SetPartial_Array( uniform_buffer_name, uniform_buffer_member_array_name.c_str(), array_index, effective_offset );
 												ImGui::PopID();
 											}
 
@@ -457,14 +464,19 @@ namespace Engine::ImGuiDrawer
 									ImGui::TableNextRow();
 							}
 
-							for( const auto& [ dont_care, uniform_buffer_member_single_info ] : uniform_buffer_info->members_single_map )
+							for( const auto& [ uniform_buffer_member_name, uniform_buffer_member_info ] : uniform_buffer_info->members_single_map )
 							{
-								ImGui::TableNextColumn(); ImGui::TextUnformatted( uniform_buffer_member_single_info->editor_name.c_str() );
+								ImGui::TableNextColumn(); ImGui::TextUnformatted( uniform_buffer_member_info->editor_name.c_str() );
 
 								ImGui::TableNextColumn();
 
-								ImGui::PushID( ( void* )uniform_buffer_member_single_info );
-								is_modified |= Draw( uniform_buffer_member_single_info->type, memory_blob + uniform_buffer_member_single_info->offset );
+								std::byte* effective_offset = memory_blob + uniform_buffer_member_info->offset;
+
+								ImGui::PushID( ( void* )uniform_buffer_member_info );
+								is_modified |= Draw( uniform_buffer_member_info->type, effective_offset );
+								/* Draw() call above modifies the memory provided but an explicit material.SetPartial() call is necessary for proper registration of the modification. */
+								if( is_modified )
+									material.SetPartial( uniform_buffer_name, uniform_buffer_member_name.c_str(), effective_offset );
 								ImGui::PopID();
 							}
 
