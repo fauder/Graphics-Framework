59453878a06829f9c0f6cc2ed0f4a8031ad2786e
fauder <canik.burak@gmail.com>
2024-09-04 21:35:14 +0300

    Engine:Core/AssetDatabase: Introduce CreateAssetFromMemory()

--- Engine/Engine/Graphics/Macros.h
+++ Engine/Engine/Graphics/Macros.h
@@ -5,6 +5,23 @@
 
 #define BUFFER_OFFSET( idx ) ( static_cast< std::byte* >( 0 ) + ( idx ) )
 
+#define ASSET_LOADER_CLASS_DECLARATION_FROM_FILE_ONLY( class_name ) \
+class Loader\
+{\
+public:\
+	Loader( const Loader& ) = delete;\
+	Loader& operator =( const Loader& ) = delete;\
+	static std::optional< class_name > FromFile( const std::string_view name, const std::string& file_path, const ImportSettings& import_settings );\
+private:\
+	Loader()\
+	{}\
+	static Loader& Instance()\
+	{\
+		static Loader instance;\
+		return instance;\
+	}\
+};
+
 #define ASSET_LOADER_CLASS_DECLARATION( class_name ) \
 class Loader\
 {\
@@ -12,6 +29,7 @@ public:\
 	Loader( const Loader& ) = delete;\
 	Loader& operator =( const Loader& ) = delete;\
 	static std::optional< class_name > FromFile( const std::string_view name, const std::string& file_path, const ImportSettings& import_settings );\
+	static std::optional< class_name > FromMemory( const std::string_view name, const std::byte* data, const int size, const ImportSettings& import_settings );\
 private:\
 	Loader()\
 	{}\
