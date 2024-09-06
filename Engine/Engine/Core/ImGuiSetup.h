7cc0ff8db7606359d2012954906783dc974e10df
fauder <canik.burak@gmail.com>
2024-09-06 22:32:34 +0300

    Vendor, Engine & Sandbox: Update ImGui to latest master (1.89.8 -> 1.92.2 WIP, commit SHA a2366f9)

--- Engine/Engine/Core/ImGuiSetup.h
+++ Engine/Engine/Core/ImGuiSetup.h
@@ -1,9 +1,9 @@
 #pragma once
 
 // Vendor/ImGui includes.
-#include "ImGui/imgui.h"
-#include "ImGui/imgui_impl_glfw.h"
-#include "ImGui/imgui_impl_opengl3.h"
+#include <ImGui/imgui.h>
+#include <ImGui/backends/imgui_impl_glfw.h>
+#include <ImGui/backends/imgui_impl_opengl3.h>
 
 namespace ImGuiSetup
 {
