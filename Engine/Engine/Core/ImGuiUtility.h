a66b1cc57b018f4f868989d300c7f1ed126ceec2
fauder <canik.burak@gmail.com>
2024-09-07 02:45:12 +0300

    Engine/Core/ImGuiUtility: Add toggle support to Begin/EndGroupPanel() API

--- Engine/Engine/Core/ImGuiUtility.h
+++ Engine/Engine/Core/ImGuiUtility.h
@@ -27,7 +27,7 @@ namespace Engine::ImGuiUtility
 						   const ImGuiCond condition = ImGuiCond_Always );
 
 	/* https://github.com/ocornut/imgui/issues/1496#issuecomment-655048353 */
-	void BeginGroupPanel( const char* name, const ImVec2& size = ImVec2( 0.0f, 0.0f ) );
+	void BeginGroupPanel( const char* name, bool* is_enabled = nullptr, const ImVec2& size = ImVec2( 0.0f, 0.0f ) );
 	/* https://github.com/ocornut/imgui/issues/1496#issuecomment-655048353 */
-	void EndGroupPanel();
+	void EndGroupPanel( bool* is_enabled = nullptr );
 }
