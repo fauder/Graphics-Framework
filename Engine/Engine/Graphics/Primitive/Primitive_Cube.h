c664be1106868bad32a3caeab41bb053e2856c26
fauder <canik.burak@gmail.com>
2024-09-22 22:47:03 +0300

    🔁 Engine/Graphics/Primitive/Primiti_Cube: Swap FRONT/BACK comments as they were mixed

--- Engine/Engine/Graphics/Primitive/Primitive_Cube.h
+++ Engine/Engine/Graphics/Primitive/Primitive_Cube.h
@@ -10,19 +10,19 @@ namespace Engine::Primitive::NonIndexed::Cube
 {
 	constexpr std::array< Vector3, 36 > Positions
 	( {
-		{ -0.5f, -0.5f, -0.5f }, // BACK
-		{  0.5f, -0.5f, -0.5f }, // BACK
-		{  0.5f,  0.5f, -0.5f }, // BACK
-		{  0.5f,  0.5f, -0.5f }, // BACK
-		{ -0.5f,  0.5f, -0.5f }, // BACK
-		{ -0.5f, -0.5f, -0.5f }, // BACK
+		{ -0.5f, -0.5f, -0.5f }, // FRONT
+		{  0.5f, -0.5f, -0.5f }, // FRONT
+		{  0.5f,  0.5f, -0.5f }, // FRONT
+		{  0.5f,  0.5f, -0.5f }, // FRONT
+		{ -0.5f,  0.5f, -0.5f }, // FRONT
+		{ -0.5f, -0.5f, -0.5f }, // FRONT
 
-		{ -0.5f, -0.5f,  0.5f }, // FRONT
-		{  0.5f, -0.5f,  0.5f }, // FRONT
-		{  0.5f,  0.5f,  0.5f }, // FRONT
-		{  0.5f,  0.5f,  0.5f }, // FRONT
-		{ -0.5f,  0.5f,  0.5f }, // FRONT
-		{ -0.5f, -0.5f,  0.5f }, // FRONT
+		{ -0.5f, -0.5f,  0.5f }, // BACK
+		{  0.5f, -0.5f,  0.5f }, // BACK
+		{  0.5f,  0.5f,  0.5f }, // BACK
+		{  0.5f,  0.5f,  0.5f }, // BACK
+		{ -0.5f,  0.5f,  0.5f }, // BACK
+		{ -0.5f, -0.5f,  0.5f }, // BACK
 
 		{ -0.5f,  0.5f,  0.5f }, // LEFT
 		{ -0.5f,  0.5f, -0.5f }, // LEFT
@@ -55,19 +55,19 @@ namespace Engine::Primitive::NonIndexed::Cube
 
 	constexpr std::array< Vector2, 36 > UVs
 	( {
-		{ 0.0f, 0.0f }, // BACK
-		{ 1.0f, 0.0f }, // BACK
-		{ 1.0f, 1.0f }, // BACK
-		{ 1.0f, 1.0f }, // BACK
-		{ 0.0f, 1.0f }, // BACK
-		{ 0.0f, 0.0f }, // BACK
-
 		{ 0.0f, 0.0f }, // FRONT
 		{ 1.0f, 0.0f }, // FRONT
 		{ 1.0f, 1.0f }, // FRONT
 		{ 1.0f, 1.0f }, // FRONT
 		{ 0.0f, 1.0f }, // FRONT
 		{ 0.0f, 0.0f }, // FRONT
+
+		{ 0.0f, 0.0f }, // BACK
+		{ 1.0f, 0.0f }, // BACK
+		{ 1.0f, 1.0f }, // BACK
+		{ 1.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f }, // BACK
+		{ 0.0f, 0.0f }, // BACK
 
 		{ 1.0f, 0.0f }, // LEFT
 		{ 1.0f, 1.0f }, // LEFT
@@ -100,19 +100,19 @@ namespace Engine::Primitive::NonIndexed::Cube
 
 	constexpr std::array< Vector2, 36 > UVs_CubeMap
 	( {
-		{ 1.0f,  0.333f	}, // BACK
-		{ 0.75f, 0.333f	}, // BACK
-		{ 0.75f, 0.666f	}, // BACK
-		{ 0.75f, 0.666f	}, // BACK
-		{ 1.0f,  0.666f	}, // BACK
-		{ 1.0f,  0.333f	}, // BACK
+		{ 1.0f,  0.333f	}, // FRONT
+		{ 0.75f, 0.333f	}, // FRONT
+		{ 0.75f, 0.666f	}, // FRONT
+		{ 0.75f, 0.666f	}, // FRONT
+		{ 1.0f,  0.666f	}, // FRONT
+		{ 1.0f,  0.333f	}, // FRONT
 
-		{ 0.25f, 0.333f	}, // FRONT
-		{ 0.5f,  0.333f	}, // FRONT
-		{ 0.5f,  0.666f	}, // FRONT
-		{ 0.5f,  0.666f	}, // FRONT
-		{ 0.25f, 0.666f	}, // FRONT
-		{ 0.25f, 0.333f	}, // FRONT
+		{ 0.25f, 0.333f	}, // BACK
+		{ 0.5f,  0.333f	}, // BACK
+		{ 0.5f,  0.666f	}, // BACK
+		{ 0.5f,  0.666f	}, // BACK
+		{ 0.25f, 0.666f	}, // BACK
+		{ 0.25f, 0.333f	}, // BACK
 
 		{ 0.25f, 0.666f	}, // LEFT
 		{ 0.0f,  0.666f	}, // LEFT
@@ -145,19 +145,19 @@ namespace Engine::Primitive::NonIndexed::Cube
 
 	constexpr std::array< Vector3, 36 > Normals
 	( {
-		{  0.0f,  0.0f, -1.0f }, // BACK
-		{  0.0f,  0.0f, -1.0f }, // BACK
-		{  0.0f,  0.0f, -1.0f }, // BACK
-		{  0.0f,  0.0f, -1.0f }, // BACK
-		{  0.0f,  0.0f, -1.0f }, // BACK
-		{  0.0f,  0.0f, -1.0f }, // BACK
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+		{  0.0f,  0.0f, -1.0f }, // FRONT
 
-		{  0.0f,  0.0f, +1.0f }, // FRONT
-		{  0.0f,  0.0f, +1.0f }, // FRONT
-		{  0.0f,  0.0f, +1.0f }, // FRONT
-		{  0.0f,  0.0f, +1.0f }, // FRONT
-		{  0.0f,  0.0f, +1.0f }, // FRONT
-		{  0.0f,  0.0f, +1.0f }, // FRONT
+		{  0.0f,  0.0f, +1.0f }, // BACK
+		{  0.0f,  0.0f, +1.0f }, // BACK
+		{  0.0f,  0.0f, +1.0f }, // BACK
+		{  0.0f,  0.0f, +1.0f }, // BACK
+		{  0.0f,  0.0f, +1.0f }, // BACK
+		{  0.0f,  0.0f, +1.0f }, // BACK
 
 		{ -1.0f,  0.0f,  0.0f }, // LEFT
 		{ -1.0f,  0.0f,  0.0f }, // LEFT
@@ -190,19 +190,19 @@ namespace Engine::Primitive::NonIndexed::Cube
 
 	constexpr std::array< Vector4, 36 > VertexColors
 	( {
-		{ 1.0f, 0.0f, 0.0f, 1.0f }, // BACK
-		{ 1.0f, 0.0f, 0.0f, 1.0f }, // BACK
-		{ 1.0f, 0.0f, 0.0f, 1.0f }, // BACK
-		{ 1.0f, 0.0f, 0.0f, 1.0f }, // BACK
-		{ 1.0f, 0.0f, 0.0f, 1.0f }, // BACK
-		{ 1.0f, 0.0f, 0.0f, 1.0f }, // BACK
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
 
-		{ 0.0f, 1.0f, 0.0f, 1.0f }, // FRONT
-		{ 0.0f, 1.0f, 0.0f, 1.0f }, // FRONT
-		{ 0.0f, 1.0f, 0.0f, 1.0f }, // FRONT
-		{ 0.0f, 1.0f, 0.0f, 1.0f }, // FRONT
-		{ 0.0f, 1.0f, 0.0f, 1.0f }, // FRONT
-		{ 0.0f, 1.0f, 0.0f, 1.0f }, // FRONT
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
 
 		{ 0.0f, 0.0f, 1.0f, 1.0f }, // LEFT
 		{ 0.0f, 0.0f, 1.0f, 1.0f }, // LEFT
