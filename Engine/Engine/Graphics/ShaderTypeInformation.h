6971a99b14e2bf66e18c2cb951049206066fbb4d
fauder <canik.burak@gmail.com>
2024-10-07 23:01:37 +0300

    Engine/Graphics/ShaderTypeInformation: Add samplers to SizeOf()

--- Engine/Engine/Graphics/ShaderTypeInformation.h
+++ Engine/Engine/Graphics/ShaderTypeInformation.h
@@ -87,9 +87,43 @@ namespace Engine::GL::Type
 			case GL_DOUBLE_MAT4x3 		: return sizeof( Matrix4x3D );
 
 			/* Texture samplers: */
-			case GL_SAMPLER_1D 			: return sizeof( int );
-			case GL_SAMPLER_2D 			: return sizeof( int );
-			case GL_SAMPLER_3D 			: return sizeof( int );
+			case GL_SAMPLER_1D:
+			case GL_SAMPLER_2D:
+			case GL_SAMPLER_3D:
+			case GL_SAMPLER_CUBE:
+			case GL_SAMPLER_1D_SHADOW:
+			case GL_SAMPLER_2D_SHADOW:
+			case GL_SAMPLER_1D_ARRAY:
+			case GL_SAMPLER_2D_ARRAY:
+			case GL_SAMPLER_1D_ARRAY_SHADOW:
+			case GL_SAMPLER_2D_ARRAY_SHADOW:
+			case GL_SAMPLER_2D_MULTISAMPLE:
+			case GL_SAMPLER_2D_MULTISAMPLE_ARRAY:
+			case GL_SAMPLER_CUBE_SHADOW:
+			case GL_SAMPLER_BUFFER:
+			case GL_SAMPLER_2D_RECT:
+			case GL_SAMPLER_2D_RECT_SHADOW:
+			case GL_INT_SAMPLER_1D:
+			case GL_INT_SAMPLER_2D:
+			case GL_INT_SAMPLER_3D:
+			case GL_INT_SAMPLER_CUBE:
+			case GL_INT_SAMPLER_1D_ARRAY:
+			case GL_INT_SAMPLER_2D_ARRAY:
+			case GL_INT_SAMPLER_2D_MULTISAMPLE:
+			case GL_INT_SAMPLER_2D_MULTISAMPLE_ARRAY:
+			case GL_INT_SAMPLER_BUFFER:
+			case GL_INT_SAMPLER_2D_RECT:
+			case GL_UNSIGNED_INT_SAMPLER_1D:
+			case GL_UNSIGNED_INT_SAMPLER_2D:
+			case GL_UNSIGNED_INT_SAMPLER_3D:
+			case GL_UNSIGNED_INT_SAMPLER_CUBE:
+			case GL_UNSIGNED_INT_SAMPLER_1D_ARRAY:
+			case GL_UNSIGNED_INT_SAMPLER_2D_ARRAY:
+			case GL_UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE:
+			case GL_UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE_ARRAY:
+			case GL_UNSIGNED_INT_SAMPLER_BUFFER:
+			case GL_UNSIGNED_INT_SAMPLER_2D_RECT:
+				return sizeof( int );
 		}
 
 		throw std::runtime_error( "ERROR::SHADER_TYPE::SizeOf() called with an unknown GL type!" );
