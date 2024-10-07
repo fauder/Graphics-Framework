7992c6eb4bf63b140835af034a5b4eebb6e065c4
fauder <canik.burak@gmail.com>
2024-10-07 23:06:00 +0300

    Engine/Graphics/Primitive/Cube_FullScreen: Add new primitive

---
+++ Engine/Engine/Graphics/Primitive/Primitive_Cube_FullScreen.h
@@ -0,0 +1,190 @@
+#pragma once
+
+// Project Includes.
+#include "Math/Vector.hpp"
+
+// std Includes.
+#include <array>
+
+namespace Engine::Primitive::NonIndexed::Cube_FullScreen
+{
+	constexpr std::array< Vector3, 36 > Positions
+	( {
+		{ -1.0f, -1.0f, -1.0f }, // FRONT
+		{  1.0f, -1.0f, -1.0f }, // FRONT
+		{  1.0f,  1.0f, -1.0f }, // FRONT
+		{  1.0f,  1.0f, -1.0f }, // FRONT
+		{ -1.0f,  1.0f, -1.0f }, // FRONT
+		{ -1.0f, -1.0f, -1.0f }, // FRONT
+
+		{ -1.0f, -1.0f,  1.0f }, // BACK
+		{  1.0f,  1.0f,  1.0f }, // BACK
+		{  1.0f, -1.0f,  1.0f }, // BACK
+		{  1.0f,  1.0f,  1.0f }, // BACK
+		{ -1.0f, -1.0f,  1.0f }, // BACK
+		{ -1.0f,  1.0f,  1.0f }, // BACK
+
+		{ -1.0f,  1.0f,  1.0f }, // LEFT
+		{ -1.0f, -1.0f, -1.0f }, // LEFT
+		{ -1.0f,  1.0f, -1.0f }, // LEFT
+		{ -1.0f, -1.0f, -1.0f }, // LEFT
+		{ -1.0f,  1.0f,  1.0f }, // LEFT
+		{ -1.0f, -1.0f,  1.0f }, // LEFT
+
+		{  1.0f,  1.0f,  1.0f }, // RIGHT
+		{  1.0f,  1.0f, -1.0f }, // RIGHT
+		{  1.0f, -1.0f, -1.0f }, // RIGHT
+		{  1.0f, -1.0f, -1.0f }, // RIGHT
+		{  1.0f, -1.0f,  1.0f }, // RIGHT
+		{  1.0f,  1.0f,  1.0f }, // RIGHT
+
+		{ -1.0f, -1.0f, -1.0f }, // BOTTOM
+		{  1.0f, -1.0f,  1.0f }, // BOTTOM
+		{  1.0f, -1.0f, -1.0f }, // BOTTOM
+		{  1.0f, -1.0f,  1.0f }, // BOTTOM
+		{ -1.0f, -1.0f, -1.0f }, // BOTTOM
+		{ -1.0f, -1.0f,  1.0f }, // BOTTOM
+
+		{ -1.0f,  1.0f, -1.0f }, // TOP
+		{  1.0f,  1.0f, -1.0f }, // TOP
+		{  1.0f,  1.0f,  1.0f }, // TOP
+		{  1.0f,  1.0f,  1.0f }, // TOP
+		{ -1.0f,  1.0f,  1.0f }, // TOP
+		{ -1.0f,  1.0f, -1.0f }	 // TOP
+	} );
+
+	constexpr std::array< Vector2, 36 > UVs
+	( {
+		{ 0.0f, 0.0f }, // FRONT
+		{ 1.0f, 0.0f }, // FRONT
+		{ 1.0f, 1.0f }, // FRONT
+		{ 1.0f, 1.0f }, // FRONT
+		{ 0.0f, 1.0f }, // FRONT
+		{ 0.0f, 0.0f }, // FRONT
+
+		{ 0.0f, 0.0f }, // BACK
+		{ 1.0f, 1.0f }, // BACK
+		{ 1.0f, 0.0f }, // BACK
+		{ 1.0f, 1.0f }, // BACK
+		{ 0.0f, 0.0f }, // BACK
+		{ 0.0f, 1.0f }, // BACK
+
+		{ 1.0f, 0.0f }, // LEFT
+		{ 0.0f, 1.0f }, // LEFT
+		{ 1.0f, 1.0f }, // LEFT
+		{ 0.0f, 1.0f }, // LEFT
+		{ 1.0f, 0.0f }, // LEFT
+		{ 0.0f, 0.0f }, // LEFT
+
+		{ 1.0f, 0.0f }, // RIGHT
+		{ 1.0f, 1.0f }, // RIGHT
+		{ 0.0f, 1.0f }, // RIGHT
+		{ 0.0f, 1.0f }, // RIGHT
+		{ 0.0f, 0.0f }, // RIGHT
+		{ 1.0f, 0.0f }, // RIGHT
+
+		{ 0.0f, 1.0f }, // BOTTOM
+		{ 1.0f, 0.0f }, // BOTTOM
+		{ 1.0f, 1.0f }, // BOTTOM
+		{ 1.0f, 0.0f }, // BOTTOM
+		{ 0.0f, 1.0f }, // BOTTOM
+		{ 0.0f, 0.0f }, // BOTTOM
+
+		{ 0.0f, 1.0f }, // TOP
+		{ 1.0f, 1.0f }, // TOP
+		{ 1.0f, 0.0f }, // TOP
+		{ 1.0f, 0.0f }, // TOP
+		{ 0.0f, 0.0f }, // TOP
+		{ 0.0f, 1.0f }  // TOP
+	} );
+
+	constexpr std::array< Vector3, 36 > Normals
+	( {
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+		{  0.0f,  0.0f, -1.0f }, // FRONT
+
+		{  0.0f,  0.0f, +1.0f }, // BACK
+		{  0.0f,  0.0f, +1.0f }, // BACK
+		{  0.0f,  0.0f, +1.0f }, // BACK
+		{  0.0f,  0.0f, +1.0f }, // BACK
+		{  0.0f,  0.0f, +1.0f }, // BACK
+		{  0.0f,  0.0f, +1.0f }, // BACK
+
+		{ -1.0f,  0.0f,  0.0f }, // LEFT
+		{ -1.0f,  0.0f,  0.0f }, // LEFT
+		{ -1.0f,  0.0f,  0.0f }, // LEFT
+		{ -1.0f,  0.0f,  0.0f }, // LEFT
+		{ -1.0f,  0.0f,  0.0f }, // LEFT
+		{ -1.0f,  0.0f,  0.0f }, // LEFT
+
+		{ +1.0f,  0.0f,  0.0f }, // RIGHT
+		{ +1.0f,  0.0f,  0.0f }, // RIGHT
+		{ +1.0f,  0.0f,  0.0f }, // RIGHT
+		{ +1.0f,  0.0f,  0.0f }, // RIGHT
+		{ +1.0f,  0.0f,  0.0f }, // RIGHT
+		{ +1.0f,  0.0f,  0.0f }, // RIGHT
+
+		{  0.0f, -1.0f,  0.0f }, // BOTTOM
+		{  0.0f, -1.0f,  0.0f }, // BOTTOM
+		{  0.0f, -1.0f,  0.0f }, // BOTTOM
+		{  0.0f, -1.0f,  0.0f }, // BOTTOM
+		{  0.0f, -1.0f,  0.0f }, // BOTTOM
+		{  0.0f, -1.0f,  0.0f }, // BOTTOM
+
+		{  0.0f, +1.0f,  0.0f }, // TOP
+		{  0.0f, +1.0f,  0.0f }, // TOP
+		{  0.0f, +1.0f,  0.0f }, // TOP
+		{  0.0f, +1.0f,  0.0f }, // TOP
+		{  0.0f, +1.0f,  0.0f }, // TOP
+		{  0.0f, +1.0f,  0.0f }  // TOP
+	} );
+
+	constexpr std::array< Vector4, 36 > VertexColors
+	( {
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+		{ 1.0f, 0.0f, 0.0f, 1.0f }, // FRONT
+
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+		{ 0.0f, 1.0f, 0.0f, 1.0f }, // BACK
+
+		{ 0.0f, 0.0f, 1.0f, 1.0f }, // LEFT
+		{ 0.0f, 0.0f, 1.0f, 1.0f }, // LEFT
+		{ 0.0f, 0.0f, 1.0f, 1.0f }, // LEFT
+		{ 0.0f, 0.0f, 1.0f, 1.0f }, // LEFT
+		{ 0.0f, 0.0f, 1.0f, 1.0f }, // LEFT
+		{ 0.0f, 0.0f, 1.0f, 1.0f }, // LEFT
+
+		{ 1.0f, 1.0f, 0.0f, 1.0f }, // RIGHT
+		{ 1.0f, 1.0f, 0.0f, 1.0f }, // RIGHT
+		{ 1.0f, 1.0f, 0.0f, 1.0f }, // RIGHT
+		{ 1.0f, 1.0f, 0.0f, 1.0f }, // RIGHT
+		{ 1.0f, 1.0f, 0.0f, 1.0f }, // RIGHT
+		{ 1.0f, 1.0f, 0.0f, 1.0f }, // RIGHT
+
+		{ 0.0f, 1.0f, 1.0f, 1.0f }, // BOTTOM
+		{ 0.0f, 1.0f, 1.0f, 1.0f }, // BOTTOM
+		{ 0.0f, 1.0f, 1.0f, 1.0f }, // BOTTOM
+		{ 0.0f, 1.0f, 1.0f, 1.0f }, // BOTTOM
+		{ 0.0f, 1.0f, 1.0f, 1.0f }, // BOTTOM
+		{ 0.0f, 1.0f, 1.0f, 1.0f }, // BOTTOM
+
+		{ 1.0f, 0.0f, 1.0f, 1.0f }, // TOP
+		{ 1.0f, 0.0f, 1.0f, 1.0f }, // TOP
+		{ 1.0f, 0.0f, 1.0f, 1.0f }, // TOP
+		{ 1.0f, 0.0f, 1.0f, 1.0f }, // TOP
+		{ 1.0f, 0.0f, 1.0f, 1.0f }, // TOP
+		{ 1.0f, 0.0f, 1.0f, 1.0f }  // TOP
+	} );
+}
