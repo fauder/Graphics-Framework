52e451b654047f2f8895574f0b8a22420502b8b4
fauder <canik.burak@gmail.com>
2024-11-23 03:29:14 +0300

    Engine/Graphics/RenderState, ModelLoader & Primitives: Change front faces from CCW to CW -> adapt Primitives & ModelLoader

--- Engine/Engine/Graphics/Primitive/Primitive_Cube.h
+++ Engine/Engine/Graphics/Primitive/Primitive_Cube.h
@@ -11,91 +11,91 @@ namespace Engine::Primitive::NonIndexed::Cube
 	constexpr std::array< Vector3, 36 > Positions
 	( {
 		{ -0.5f, -0.5f, -0.5f }, // FRONT
+		{  0.5f,  0.5f, -0.5f }, // FRONT
 		{  0.5f, -0.5f, -0.5f }, // FRONT
 		{  0.5f,  0.5f, -0.5f }, // FRONT
-		{  0.5f,  0.5f, -0.5f }, // FRONT
-		{ -0.5f,  0.5f, -0.5f }, // FRONT
 		{ -0.5f, -0.5f, -0.5f }, // FRONT
+		{ -0.5f,  0.5f, -0.5f }, // FRONT
 
 		{ -0.5f, -0.5f,  0.5f }, // BACK
-		{  0.5f,  0.5f,  0.5f }, // BACK
 		{  0.5f, -0.5f,  0.5f }, // BACK
 		{  0.5f,  0.5f,  0.5f }, // BACK
-		{ -0.5f, -0.5f,  0.5f }, // BACK
+		{  0.5f,  0.5f,  0.5f }, // BACK
 		{ -0.5f,  0.5f,  0.5f }, // BACK
+		{ -0.5f, -0.5f,  0.5f }, // BACK
 
 		{ -0.5f,  0.5f,  0.5f }, // LEFT
-		{ -0.5f, -0.5f, -0.5f }, // LEFT
 		{ -0.5f,  0.5f, -0.5f }, // LEFT
 		{ -0.5f, -0.5f, -0.5f }, // LEFT
-		{ -0.5f,  0.5f,  0.5f }, // LEFT
+		{ -0.5f, -0.5f, -0.5f }, // LEFT
 		{ -0.5f, -0.5f,  0.5f }, // LEFT
+		{ -0.5f,  0.5f,  0.5f }, // LEFT
 
 		{  0.5f,  0.5f,  0.5f }, // RIGHT
+		{  0.5f, -0.5f, -0.5f }, // RIGHT
 		{  0.5f,  0.5f, -0.5f }, // RIGHT
 		{  0.5f, -0.5f, -0.5f }, // RIGHT
-		{  0.5f, -0.5f, -0.5f }, // RIGHT
-		{  0.5f, -0.5f,  0.5f }, // RIGHT
 		{  0.5f,  0.5f,  0.5f }, // RIGHT
+		{  0.5f, -0.5f,  0.5f }, // RIGHT
 
 		{ -0.5f, -0.5f, -0.5f }, // BOTTOM
-		{  0.5f, -0.5f,  0.5f }, // BOTTOM
 		{  0.5f, -0.5f, -0.5f }, // BOTTOM
 		{  0.5f, -0.5f,  0.5f }, // BOTTOM
-		{ -0.5f, -0.5f, -0.5f }, // BOTTOM
+		{  0.5f, -0.5f,  0.5f }, // BOTTOM
 		{ -0.5f, -0.5f,  0.5f }, // BOTTOM
+		{ -0.5f, -0.5f, -0.5f }, // BOTTOM
 
 		{ -0.5f,  0.5f, -0.5f }, // TOP
+		{  0.5f,  0.5f,  0.5f }, // TOP
 		{  0.5f,  0.5f, -0.5f }, // TOP
 		{  0.5f,  0.5f,  0.5f }, // TOP
-		{  0.5f,  0.5f,  0.5f }, // TOP
-		{ -0.5f,  0.5f,  0.5f }, // TOP
-		{ -0.5f,  0.5f, -0.5f }	 // TOP
+		{ -0.5f,  0.5f, -0.5f }, // TOP
+		{ -0.5f,  0.5f,  0.5f }  // TOP
 	} );
 
 	constexpr std::array< Vector2, 36 > UVs
 	( {
 		{ 0.0f, 0.0f }, // FRONT
+		{ 1.0f, 1.0f }, // FRONT
 		{ 1.0f, 0.0f }, // FRONT
 		{ 1.0f, 1.0f }, // FRONT
-		{ 1.0f, 1.0f }, // FRONT
-		{ 0.0f, 1.0f }, // FRONT
 		{ 0.0f, 0.0f }, // FRONT
+		{ 0.0f, 1.0f }, // FRONT
 
 		{ 0.0f, 0.0f }, // BACK
-		{ 1.0f, 1.0f }, // BACK
 		{ 1.0f, 0.0f }, // BACK
 		{ 1.0f, 1.0f }, // BACK
-		{ 0.0f, 0.0f }, // BACK
+		{ 1.0f, 1.0f }, // BACK
 		{ 0.0f, 1.0f }, // BACK
+		{ 0.0f, 0.0f }, // BACK
 
 		{ 1.0f, 0.0f }, // LEFT
-		{ 0.0f, 1.0f }, // LEFT
 		{ 1.0f, 1.0f }, // LEFT
 		{ 0.0f, 1.0f }, // LEFT
-		{ 1.0f, 0.0f }, // LEFT
+		{ 0.0f, 1.0f }, // LEFT
 		{ 0.0f, 0.0f }, // LEFT
+		{ 1.0f, 0.0f }, // LEFT
 
 		{ 1.0f, 0.0f }, // RIGHT
+		{ 0.0f, 1.0f }, // RIGHT
 		{ 1.0f, 1.0f }, // RIGHT
 		{ 0.0f, 1.0f }, // RIGHT
-		{ 0.0f, 1.0f }, // RIGHT
-		{ 0.0f, 0.0f }, // RIGHT
 		{ 1.0f, 0.0f }, // RIGHT
+		{ 0.0f, 0.0f }, // RIGHT
 
 		{ 0.0f, 1.0f }, // BOTTOM
-		{ 1.0f, 0.0f }, // BOTTOM
 		{ 1.0f, 1.0f }, // BOTTOM
 		{ 1.0f, 0.0f }, // BOTTOM
-		{ 0.0f, 1.0f }, // BOTTOM
+		{ 1.0f, 0.0f }, // BOTTOM
 		{ 0.0f, 0.0f }, // BOTTOM
+		{ 0.0f, 1.0f }, // BOTTOM
 
 		{ 0.0f, 1.0f }, // TOP
+		{ 1.0f, 0.0f }, // TOP
 		{ 1.0f, 1.0f }, // TOP
 		{ 1.0f, 0.0f }, // TOP
-		{ 1.0f, 0.0f }, // TOP
-		{ 0.0f, 0.0f }, // TOP
-		{ 0.0f, 1.0f }  // TOP
+		{ 0.0f, 1.0f }, // TOP
+		{ 0.0f, 0.0f }  // TOP
 	} );
 
 	constexpr std::array< Vector3, 36 > Normals
--- Engine/Engine/Graphics/Primitive/Primitive_Quad.h
+++ Engine/Engine/Graphics/Primitive/Primitive_Quad.h
@@ -11,21 +11,21 @@ namespace Engine::Primitive::NonIndexed::Quad
 	constexpr std::array< Vector3, 6 > Positions
 	( {
 		{ -0.5f, -0.5f, 0.0f }, // FRONT
+		{  0.5f,  0.5f, 0.0f }, // FRONT
 		{  0.5f, -0.5f, 0.0f }, // FRONT
 		{  0.5f,  0.5f, 0.0f }, // FRONT
-		{  0.5f,  0.5f, 0.0f }, // FRONT
-		{ -0.5f,  0.5f, 0.0f }, // FRONT
-		{ -0.5f, -0.5f, 0.0f }  // FRONT
+		{ -0.5f, -0.5f, 0.0f }, // FRONT
+		{ -0.5f,  0.5f, 0.0f }  // FRONT
 	} );
 
 	constexpr std::array< Vector2, 6 > UVs
 	( {
 		{ 0.0f, 0.0f }, // FRONT
+		{ 1.0f, 1.0f }, // FRONT
 		{ 1.0f, 0.0f }, // FRONT
 		{ 1.0f, 1.0f }, // FRONT
-		{ 1.0f, 1.0f }, // FRONT
-		{ 0.0f, 1.0f }, // FRONT
-		{ 0.0f, 0.0f }  // FRONT
+		{ 0.0f, 0.0f }, // FRONT
+		{ 0.0f, 1.0f }  // FRONT
 	} );
 
 	constexpr std::array< Vector3, 6 > Normals
