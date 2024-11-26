d4e58548ab843b68c88e9c7fc3ee6d9cb9c18c5a
fauder <canik.burak@gmail.com>
2024-11-26 17:54:17 +0300

    Engine/Graphics/Primitive/Cube Add Tangent & Bitangent attributes

--- Engine/Engine/Graphics/Primitive/Primitive_Cube.h
+++ Engine/Engine/Graphics/Primitive/Primitive_Cube.h
@@ -95,7 +95,7 @@ namespace Engine::Primitive::NonIndexed::Cube
 		{ 1.0f, 0.0f }, // TOP
 		{ 1.0f, 1.0f }, // TOP
 		{ 0.0f, 0.0f }, // TOP
-		{ 0.0f, 1.0f } // TOP
+		{ 0.0f, 1.0f }  // TOP
 	} );
 
 	constexpr std::array< Vector3, 36 > Normals
@@ -141,6 +141,96 @@ namespace Engine::Primitive::NonIndexed::Cube
 		{  0.0f, +1.0f,  0.0f }, // TOP
 		{  0.0f, +1.0f,  0.0f }, // TOP
 		{  0.0f, +1.0f,  0.0f }  // TOP
+	} );
+
+	constexpr std::array< Vector3, 36 > Tangents
+	( {
+		{ +1.0f,  0.0f,  0.0f }, // FRONT
+		{ +1.0f,  0.0f,  0.0f }, // FRONT
+		{ +1.0f,  0.0f,  0.0f }, // FRONT
+		{ +1.0f,  0.0f,  0.0f }, // FRONT
+		{ +1.0f,  0.0f,  0.0f }, // FRONT
+		{ +1.0f,  0.0f,  0.0f }, // FRONT
+
+		{ +1.0f,  0.0f,  0.0f }, // BACK
+		{ +1.0f,  0.0f,  0.0f }, // BACK
+		{ +1.0f,  0.0f,  0.0f }, // BACK
+		{ +1.0f,  0.0f,  0.0f }, // BACK
+		{ +1.0f,  0.0f,  0.0f }, // BACK
+		{ +1.0f,  0.0f,  0.0f }, // BACK
+
+		{  0.0f,  0.0f, -1.0f }, // LEFT
+		{  0.0f,  0.0f, -1.0f }, // LEFT
+		{  0.0f,  0.0f, -1.0f }, // LEFT
+		{  0.0f,  0.0f, -1.0f }, // LEFT
+		{  0.0f,  0.0f, -1.0f }, // LEFT
+		{  0.0f,  0.0f, -1.0f }, // LEFT
+
+		{  0.0f,  0.0f, +1.0f }, // RIGHT
+		{  0.0f,  0.0f, +1.0f }, // RIGHT
+		{  0.0f,  0.0f, +1.0f }, // RIGHT
+		{  0.0f,  0.0f, +1.0f }, // RIGHT
+		{  0.0f,  0.0f, +1.0f }, // RIGHT
+		{  0.0f,  0.0f, +1.0f }, // RIGHT
+
+		{ +1.0f,  0.0f,  0.0f }, // BOTTOM
+		{ +1.0f,  0.0f,  0.0f }, // BOTTOM
+		{ +1.0f,  0.0f,  0.0f }, // BOTTOM
+		{ +1.0f,  0.0f,  0.0f }, // BOTTOM
+		{ +1.0f,  0.0f,  0.0f }, // BOTTOM
+		{ +1.0f,  0.0f,  0.0f }, // BOTTOM
+
+		{ +1.0f,  0.0f,  0.0f }, // TOP
+		{ +1.0f,  0.0f,  0.0f }, // TOP
+		{ +1.0f,  0.0f,  0.0f }, // TOP
+		{ +1.0f,  0.0f,  0.0f }, // TOP
+		{ +1.0f,  0.0f,  0.0f }, // TOP
+		{ +1.0f,  0.0f,  0.0f }  // TOP
+	} );
+
+	constexpr std::array< Vector3, 36 > Bitangents
+	( {
+		{  0.0f, +1.0f,  0.0f }, // FRONT
+		{  0.0f, +1.0f,  0.0f }, // FRONT
+		{  0.0f, +1.0f,  0.0f }, // FRONT
+		{  0.0f, +1.0f,  0.0f }, // FRONT
+		{  0.0f, +1.0f,  0.0f }, // FRONT
+		{  0.0f, +1.0f,  0.0f }, // FRONT
+
+		{  0.0f, -1.0f,  0.0f }, // BACK
+		{  0.0f, -1.0f,  0.0f }, // BACK
+		{  0.0f, -1.0f,  0.0f }, // BACK
+		{  0.0f, -1.0f,  0.0f }, // BACK
+		{  0.0f, -1.0f,  0.0f }, // BACK
+		{  0.0f, -1.0f,  0.0f }, // BACK
+
+		{  0.0f, +1.0f,  0.0f }, // LEFT
+		{  0.0f, +1.0f,  0.0f }, // LEFT
+		{  0.0f, +1.0f,  0.0f }, // LEFT
+		{  0.0f, +1.0f,  0.0f }, // LEFT
+		{  0.0f, +1.0f,  0.0f }, // LEFT
+		{  0.0f, +1.0f,  0.0f }, // LEFT
+
+		{  0.0f, +1.0f,  0.0f }, // RIGHT
+		{  0.0f, +1.0f,  0.0f }, // RIGHT
+		{  0.0f, +1.0f,  0.0f }, // RIGHT
+		{  0.0f, +1.0f,  0.0f }, // RIGHT
+		{  0.0f, +1.0f,  0.0f }, // RIGHT
+		{  0.0f, +1.0f,  0.0f }, // RIGHT
+
+		{  0.0f,  0.0f, -1.0f }, // BOTTOM
+		{  0.0f,  0.0f, -1.0f }, // BOTTOM
+		{  0.0f,  0.0f, -1.0f }, // BOTTOM
+		{  0.0f,  0.0f, -1.0f }, // BOTTOM
+		{  0.0f,  0.0f, -1.0f }, // BOTTOM
+		{  0.0f,  0.0f, -1.0f }, // BOTTOM
+
+		{  0.0f,  0.0f, +1.0f }, // TOP
+		{  0.0f,  0.0f, +1.0f }, // TOP
+		{  0.0f,  0.0f, +1.0f }, // TOP
+		{  0.0f,  0.0f, +1.0f }, // TOP
+		{  0.0f,  0.0f, +1.0f }, // TOP
+		{  0.0f,  0.0f, +1.0f }  // TOP
 	} );
 
 	constexpr std::array< Vector4, 36 > VertexColors
