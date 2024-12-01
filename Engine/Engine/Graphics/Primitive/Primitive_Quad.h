dba823ea0531b3d293c74fe76d483cb58399003e
fauder <canik.burak@gmail.com>
2024-12-01 20:55:19 +0300

    Engine/Graphics/Primitive/Quad: Add indexed attributes as well

--- Engine/Engine/Graphics/Primitive/Primitive_Quad.h
+++ Engine/Engine/Graphics/Primitive/Primitive_Quad.h
@@ -10,41 +10,82 @@ namespace Engine::Primitive::NonIndexed::Quad
 {
 	constexpr std::array< Vector3, 6 > Positions
 	( {
-		{ -0.5f, -0.5f,  0.0f }, // FRONT
-		{  0.5f,  0.5f,  0.0f }, // FRONT
-		{  0.5f, -0.5f,  0.0f }, // FRONT
-		{  0.5f,  0.5f,  0.0f }, // FRONT
-		{ -0.5f, -0.5f,  0.0f }, // FRONT
-		{ -0.5f,  0.5f,  0.0f }  // FRONT
+		{ -0.5f, -0.5f,  0.0f },
+		{  0.5f,  0.5f,  0.0f },
+		{  0.5f, -0.5f,  0.0f },
+		{  0.5f,  0.5f,  0.0f },
+		{ -0.5f, -0.5f,  0.0f },
+		{ -0.5f,  0.5f,  0.0f }
 	} );
 
 	constexpr std::array< Vector2, 6 > UVs
 	( {
-		{ 0.0f, 0.0f }, // FRONT
-		{ 1.0f, 1.0f }, // FRONT
-		{ 1.0f, 0.0f }, // FRONT
-		{ 1.0f, 1.0f }, // FRONT
-		{ 0.0f, 0.0f }, // FRONT
-		{ 0.0f, 1.0f }  // FRONT
+		{ 0.0f, 0.0f },
+		{ 1.0f, 1.0f },
+		{ 1.0f, 0.0f },
+		{ 1.0f, 1.0f },
+		{ 0.0f, 0.0f },
+		{ 0.0f, 1.0f }
 	} );
 
 	constexpr std::array< Vector3, 6 > Tangents
 	( {
-		{ +1.0f,  0.0f,  0.0f }, // FRONT
-		{ +1.0f,  0.0f,  0.0f }, // FRONT
-		{ +1.0f,  0.0f,  0.0f }, // FRONT
-		{ +1.0f,  0.0f,  0.0f }, // FRONT
-		{ +1.0f,  0.0f,  0.0f }, // FRONT
-		{ +1.0f,  0.0f,  0.0f }  // FRONT
-	  } );
+		{ +1.0f,  0.0f,  0.0f },
+		{ +1.0f,  0.0f,  0.0f },
+		{ +1.0f,  0.0f,  0.0f },
+		{ +1.0f,  0.0f,  0.0f },
+		{ +1.0f,  0.0f,  0.0f },
+		{ +1.0f,  0.0f,  0.0f }
+	} );
 
 	constexpr std::array< Vector3, 6 > Normals
 	( {
-		{  0.0f,  0.0f, -1.0f }, // FRONT
-		{  0.0f,  0.0f, -1.0f }, // FRONT
-		{  0.0f,  0.0f, -1.0f }, // FRONT
-		{  0.0f,  0.0f, -1.0f }, // FRONT
-		{  0.0f,  0.0f, -1.0f }, // FRONT
-		{  0.0f,  0.0f, -1.0f }  // FRONT
+		{  0.0f,  0.0f, -1.0f },
+		{  0.0f,  0.0f, -1.0f },
+		{  0.0f,  0.0f, -1.0f },
+		{  0.0f,  0.0f, -1.0f },
+		{  0.0f,  0.0f, -1.0f },
+		{  0.0f,  0.0f, -1.0f }
+	} );
+}
+
+namespace Engine::Primitive::Indexed::Quad
+{
+	constexpr std::array< Vector3, 4 > Positions
+	( {
+		{ -0.5f, -0.5f,  0.0f },
+		{ -0.5f,  0.5f,  0.0f },
+		{  0.5f,  0.5f,  0.0f },
+		{  0.5f, -0.5f,  0.0f }
+	} );
+
+	constexpr std::array< std::uint32_t, 6 > Indices
+	( {
+		0, 1, 2,
+		0, 2, 3
+	} );
+
+	constexpr std::array< Vector2, 4 > UVs
+	( {
+		{ 0.0f, 0.0f },
+		{ 0.0f, 1.0f },
+		{ 1.0f, 1.0f },
+		{ 1.0f, 0.0f }
+	} );
+
+	constexpr std::array< Vector3, 4 > Tangents
+	( {
+		{ +1.0f,  0.0f,  0.0f },
+		{ +1.0f,  0.0f,  0.0f },
+		{ +1.0f,  0.0f,  0.0f },
+		{ +1.0f,  0.0f,  0.0f }
+	} );
+
+	constexpr std::array< Vector3, 4 > Normals
+	( {
+		{  0.0f,  0.0f, -1.0f },
+		{  0.0f,  0.0f, -1.0f },
+		{  0.0f,  0.0f, -1.0f },
+		{  0.0f,  0.0f, -1.0f }
 	} );
 }
