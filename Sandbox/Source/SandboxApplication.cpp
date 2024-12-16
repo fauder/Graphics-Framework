fauder/Graphics-Framework

commit 8baed4596074f1189a77fc2a32934712cef1607a

🐞 Engine/Asset/Shader/Blinn-Phong.vert & Sandbox app: Work out kinks in the shader & create a rotating parallax cube

Sandbox/Source/SandboxApplication.cpp
@@ -88,23 +88,29 @@ void SandboxApplication::Initialize()
 																						.min_filter      = Engine::Texture::Filtering::Linear,
 																						.flip_vertically = false,
 																					} );
-
+		
 	container_texture_diffuse_map  = Engine::AssetDatabase< Engine::Texture >::CreateAssetFromFile( "Container (Diffuse) Map",	R"(Asset/Texture/container2.png)" );
 	container_texture_specular_map = Engine::AssetDatabase< Engine::Texture >::CreateAssetFromFile( "Container (Specular) Map", R"(Asset/Texture/container2_specular.png)" );
 
-	brickwall_diffuse_map  = Engine::AssetDatabase< Engine::Texture >::CreateAssetFromFile( "Brickwall (Diffuse) Map", R"(Asset/Texture/brickwall.jpg)",
+	brickwall_diffuse_map  = Engine::AssetDatabase< Engine::Texture >::CreateAssetFromFile( "Brickwall (Diffuse) Map", R"(Asset/Texture/bricks2.jpg)",
 																							Engine::Texture::ImportSettings
 																							{
 																								.wrap_u = Engine::Texture::Wrapping::Repeat,
 																								.wrap_v = Engine::Texture::Wrapping::Repeat
 																							} );
-	brickwall_normal_map   = Engine::AssetDatabase< Engine::Texture >::CreateAssetFromFile( "Brickwall (Normal) Map",  R"(Asset/Texture/brickwall_normal.jpg)",
+	brickwall_normal_map   = Engine::AssetDatabase< Engine::Texture >::CreateAssetFromFile( "Brickwall (Normal) Map",  R"(Asset/Texture/bricks2_normal.jpg)",
 																							Engine::Texture::ImportSettings
 																							{
 																								.wrap_u  = Engine::Texture::Wrapping::Repeat,
 																								.wrap_v  = Engine::Texture::Wrapping::Repeat,
 																								.is_sRGB = false,
 																							} );
+
+	brickwall_displacement_map = Engine::AssetDatabase< Engine::Texture >::CreateAssetFromFile( "Brickwall (Displacement) Map", R"(Asset/Texture/bricks2_disp.jpg)",
+																								Engine::Texture::ImportSettings
+																								{
+																									.is_sRGB = false,
+																								} );
 
 	transparent_window_texture = Engine::AssetDatabase< Engine::Texture >::CreateAssetFromFile( "Transparent Window", R"(Asset/Texture/blending_transparent_window.png)" );
 	
@@ -168,6 +174,8 @@ void SandboxApplication::Initialize()
 		.SetScaling( 30.0f, 15.0f, 1.0f )
 		.SetRotation( 180.0_deg, 0.0_deg, 0.0_deg )
 		.SetTranslation( 0.0f, 7.5f, -30.0f );
+
+	cube_parallax_transform.SetTranslation( -1.0f, 0.5f, -2.0f );
 
 	/* Keep the first 10 the same as the ones from LOGL: */
 	for( auto cube_index = 0; cube_index < CUBE_REFLECTED_COUNT; cube_index++ )
@@ -324,6 +332,9 @@ void SandboxApplication::Initialize()
 										  nullptr /* => No Transform here, as we will provide the Transforms as instance data. */, true /* => has shadows. */ );
 	renderer.AddRenderable( &cube_renderable, Engine::Renderer::QUEUE_ID_GEOMETRY_OUTLINED );
 
+	cube_parallax_renderable = Engine::Renderable( &cube_mesh, &wall_material, &cube_parallax_transform, true /* => has shadows. */ );
+	renderer.AddRenderable( &cube_parallax_renderable, Engine::Renderer::QUEUE_ID_GEOMETRY_OUTLINED );
+
 	cube_reflected_renderable = Engine::Renderable( &cube_reflected_mesh_instanced, &cube_reflected_material,
 													nullptr /* => No Transform here, as we will provide the Transforms as instance data. */, true /* => has shadows. */ );
 	renderer.AddRenderable( &cube_reflected_renderable, Engine::Renderer::QUEUE_ID_GEOMETRY_OUTLINED );
@@ -454,6 +465,23 @@ void SandboxApplication::Update()
 	}
 
 	cube_mesh_instanced_with_color.UpdateInstanceData( light_source_instance_data_array.data() );
+
+	/* Instanced cube's transform: */
+	{
+		Radians old_heading, old_pitch, old_bank;
+		Engine::Math::QuaternionToEuler( cube_transform_array[ 0 ].GetRotation(), old_heading, old_pitch, old_bank );
+		cube_transform_array[ 0 ].SetRotation( old_heading + angle_increment * 0.0035f, old_pitch, old_bank );
+		cube_instance_data_array[ 0 ] = cube_transform_array[ 0 ].GetFinalMatrix().Transposed(); // Vertex attribute matrices' major can not be flipped in GLSL.
+		
+		cube_mesh_instanced.UpdateInstanceData( cube_instance_data_array.data() );
+	}
+
+	/* Parallax cube's transform: */
+	{
+		Radians old_heading, old_pitch, old_bank;
+		Engine::Math::QuaternionToEuler( cube_parallax_transform.GetRotation(), old_heading, old_pitch, old_bank );
+		cube_parallax_transform.SetRotation( old_heading + angle_increment * 0.0035f, old_pitch, old_bank );
+	}
 
 	/* Camera transform: */
 	if( camera_animation_is_enabled )
@@ -1051,13 +1079,16 @@ void SandboxApplication::ResetMaterialData()
 	Vector4 ground_texture_scale_and_offset( ground_quad_scale.X(), ground_quad_scale.Y() /* Offset is 0 so no need to set it explicitly. */ );
 	ground_material.Set( "uniform_texture_scale_and_offset", ground_texture_scale_and_offset );
 
-	wall_material = Engine::Material( "Wall", shader_blinn_phong_shadowed );
+	wall_material = Engine::Material( "Wall", shader_blinn_phong_shadowed_parallax );
 	wall_material.SetTexture( "uniform_diffuse_map_slot", brickwall_diffuse_map );
 	wall_material.SetTexture( "uniform_specular_map_slot", checker_pattern_texture );
 	wall_material.SetTexture( "uniform_normal_map_slot", brickwall_normal_map );
+	wall_material.SetTexture( "uniform_parallax_height_map_slot", brickwall_displacement_map );
 	const auto& front_wall_quad_scale( wall_front_transform.GetScaling().XY() / 10.0f );
 	Vector4 front_wall_texture_scale_and_offset( front_wall_quad_scale /* Offset is 0 so no need to set it explicitly. */ );
 	wall_material.Set( "uniform_texture_scale_and_offset", front_wall_texture_scale_and_offset );
+	wall_material.Set( "uniform_texture_scale_and_offset", Vector4( 1.0f, 1.0f, 0.0f, 0.0f ) );
+	wall_material.Set( "uniform_parallax_height_scale", 0.1f );
 
 	window_material = Engine::Material( "Transparent Window", shader_basic_textured );
 	window_material.SetTexture( "uniform_texture_slot", transparent_window_texture );

Engine/Engine/Asset/Shader/Blinn-Phong.frag
@@ -16,6 +16,10 @@ in VS_To_FS
 
     mat3x3 tangent_to_view_space_transformation;
 
+#ifdef PARALLAX_MAPPING_ENABLED
+    vec3 viewing_direction_tangent_space;
+#endif
+
 #ifdef SHADOWS_ENABLED
     vec4 position_light_directional_clip_space;
 #endif
@@ -196,10 +200,10 @@ vec3 CalculateColorFromSpotLight( const int spot_light_index,
 }
 
 #ifdef PARALLAX_MAPPING_ENABLED
-vec2 ParallaxMappedTextureCoordinates( vec2 original_texture_coordinates, vec3 view_direction_tangent_space )
+vec2 ParallaxMappedTextureCoordinates( vec2 original_texture_coordinates, vec3 viewing_direction_tangent_space )
 {
 	float height_sample = texture( uniform_parallax_height_map_slot, original_texture_coordinates ).r;
-	vec2 p = view_direction_tangent_space.xy / view_direction_tangent_space.z * height_sample * uniform_parallax_height_scale;
+	vec2 p = viewing_direction_tangent_space.xy / viewing_direction_tangent_space.z * ( height_sample * uniform_parallax_height_scale );
 	return original_texture_coordinates - p;
 }
 #endif
@@ -210,7 +214,8 @@ void main()
 	vec4 viewing_direction_view_space = normalize( -fs_in.position_view_space ); 
 
 #ifdef PARALLAX_MAPPING_ENABLED
-	vec2 uvs = ParallaxMappedTextureCoordinates( fs_in.tex_coords, normalize( fs_in.tangent_to_view_space_transformation * viewing_direction_view_space.xyz ) );
+	vec2 uvs = ParallaxMappedTextureCoordinates( fs_in.tex_coords, normalize( fs_in.viewing_direction_tangent_space ) );
+	
 #else
 	vec2 uvs = fs_in.tex_coords;
 #endif
@@ -219,13 +224,13 @@ void main()
 	 * tangent_to_view_space_transformation is constructed in the vertex shader part of this shader program => it is column-major.
 	 * That's why in the below line, the sampled tangent-space normal vector is pre-multiplied by the column-major tangent_to_view_space_transformation matrix. */
 
-	vec4 normal_sample_view_space = vec4( normalize( fs_in.tangent_to_view_space_transformation * ( texture( uniform_normal_map_slot, uvs ).xyz * 2.0f - 1.0f ) ),
+    vec4 normal_sample_view_space = vec4( normalize( fs_in.tangent_to_view_space_transformation * ( texture( uniform_normal_map_slot, uvs ).xyz * 2.0f - 1.0f ) ),
 										  0.0f );
 
 //	vec4 surface_normal_view_space = normalize( fs_in.surface_normal_view_space );
 
 	vec3 diffuse_sample  = uniform_blinn_phong_material_data.has_texture_diffuse
-							   ? vec3( texture( uniform_diffuse_map_slot,  uvs ) )
+							   ? vec3( texture( uniform_diffuse_map_slot, uvs ) )
 							   : uniform_blinn_phong_material_data.color_diffuse;
 	vec3 specular_sample = vec3( texture( uniform_specular_map_slot, uvs ) );
 

Engine/Engine/Asset/Shader/Blinn-Phong.vert
@@ -7,6 +7,7 @@
 
 #pragma feature INSTANCING_ENABLED
 #pragma feature SHADOWS_ENABLED
+#pragma feature PARALLAX_MAPPING_ENABLED
 
 POSITION  vec3 position;
 NORMAL    vec3 normal;
@@ -23,6 +24,10 @@ out VS_To_FS
     vec2 tex_coords;
 
     mat3x3 tangent_to_view_space_transformation;
+
+#ifdef PARALLAX_MAPPING_ENABLED
+    vec3 viewing_direction_tangent_space;
+#endif
 
 #ifdef SHADOWS_ENABLED
     vec4 position_light_directional_clip_space;
@@ -57,19 +62,29 @@ void main()
     vs_out.surface_normal_view_space = vec4( normalize( normal * world_view_transform_for_normals ), 0.0 );
     vs_out.tex_coords                = tex_coords * uniform_texture_scale_and_offset.xy + uniform_texture_scale_and_offset.zw;
 
-    /* Tangent space is right handed (tangent points in the u direction, bitangent points in the v direction.
-     * Right-handed cross product of these produce the normal.
-     * Therefore, bitangent = normal x tangent. */
+/* Tangent Space: */
 
-    /* Normal    => */ vs_out.tangent_to_view_space_transformation[ 2 ] = vs_out.surface_normal_view_space.xyz;
-    /* Tangent   => */ vs_out.tangent_to_view_space_transformation[ 0 ] = normalize( tangent * mat3x3( world_view_transform ) );
+    /* Tangent   (view-space) => */ vs_out.tangent_to_view_space_transformation[ 0 ] = normalize( tangent * mat3x3( world_view_transform ) );
+    /* Normal    (view-space) => */ vs_out.tangent_to_view_space_transformation[ 2 ] = vs_out.surface_normal_view_space.xyz;
 
-    /* Gram-Schmidt re-orthogonalization: */
+    /* Gram-Schmidt re-orthogonalization of the tangent: */
     vs_out.tangent_to_view_space_transformation[ 0 ] = normalize( vs_out.tangent_to_view_space_transformation[ 0 ] - 
-                                                                  dot( vs_out.tangent_to_view_space_transformation[ 0 ], vs_out.surface_normal_view_space.xyz ) *
-                                                                  vs_out.surface_normal_view_space.xyz );
+                                                                  dot( vs_out.tangent_to_view_space_transformation[ 0 ], vs_out.tangent_to_view_space_transformation[ 2 ] ) *
+                                                                  vs_out.tangent_to_view_space_transformation[ 2 ] );
 
-    /* Bitangent => */ vs_out.tangent_to_view_space_transformation[ 1 ] = normalize( cross( vs_out.tangent_to_view_space_transformation[ 0 ], vs_out.surface_normal_view_space.xyz ) );
+    /* Tangent space is right handed (tangent points in the u direction, bitangent points in the v direction.
+     * BUT to construct a tangent-to-view transformation matrix, we need to find tangent space cardinal axes IN view space. 
+     * Therefore, the bitangent cardinal axis of this basis is the left handed cross product of cross( tangent, normal ). */
+    /* Bitangent (view-space) => */ vs_out.tangent_to_view_space_transformation[ 1 ] = cross( vs_out.tangent_to_view_space_transformation[ 0 ],
+                                                                                              vs_out.tangent_to_view_space_transformation[ 2 ] );
+
+#ifdef PARALLAX_MAPPING_ENABLED
+    /* Faster way of transposing tangent_to_view_space_transformation: utilize the dot products: */
+    vec3 viewing_direction_view_space = normalize( -vs_out.position_view_space.xyz );
+    vs_out.viewing_direction_tangent_space = vec3( dot( vs_out.tangent_to_view_space_transformation[ 0 ], viewing_direction_view_space ),
+                                                   dot( vs_out.tangent_to_view_space_transformation[ 1 ], viewing_direction_view_space ),
+                                                   dot( vs_out.tangent_to_view_space_transformation[ 2 ], viewing_direction_view_space ) );
+#endif
 
     gl_Position = vs_out.position_view_space * _INTRINSIC_TRANSFORM_PROJECTION;
 }

Sandbox/Source/SandboxApplication.h
@@ -90,6 +90,8 @@ private:
 	Engine::Renderable cube_renderable;
 	Engine::Renderable cube_renderable_outline;
 
+	Engine::Renderable cube_parallax_renderable;
+
 	Engine::Renderable cube_reflected_renderable;
 
 	Engine::Renderable* meteorite_renderable;
@@ -120,6 +122,7 @@ private:
 
 	Engine::Texture* brickwall_diffuse_map;
 	Engine::Texture* brickwall_normal_map;
+	Engine::Texture* brickwall_displacement_map;
 
 	Engine::Texture* half_gray_srgb_texture;
 
@@ -205,6 +208,7 @@ private:
 	std::vector< Engine::Transform > cube_transform_array;
 	std::vector< Engine::Transform > cube_reflected_transform_array;
 
+	Engine::Transform cube_parallax_transform;
 	Engine::Transform ground_transform;
 	Engine::Transform wall_front_transform;
 	Engine::Transform wall_left_transform;

commit 51112b6f07182b5883085111cf5093583887a57e

⚡ Sandbox app: Optimize to get back performance lost due to regression in 8baed45
Regression was due to uploading ALL cube instance data every frame just to rotate the first cube.
Fix was to of course to a partial upload to upload just 1 cube instance per-frame.

Sandbox/Source/SandboxApplication.cpp
@@ -473,7 +473,8 @@ void SandboxApplication::Update()
 		cube_transform_array[ 0 ].SetRotation( old_heading + angle_increment * time_delta, old_pitch, old_bank );
 		cube_instance_data_array[ 0 ] = cube_transform_array[ 0 ].GetFinalMatrix().Transposed(); // Vertex attribute matrices' major can not be flipped in GLSL.
 		
-		cube_mesh_instanced.UpdateInstanceData( cube_instance_data_array.data() );
+		cube_mesh_instanced.UpdateInstanceData_Partial( std::span< std::byte, sizeof( Matrix4x4 ) >( reinterpret_cast< std::byte* >( &cube_instance_data_array[ 0 ] ), reinterpret_cast< std::byte* >( &cube_instance_data_array[ 1 ] ) ),
+														0 );
 	}
 
 	/* Parallax cube's transform: */